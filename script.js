b892ba32356f80ce0f0b5fd69893307aea7b455e
Daniel Alcantara <112096248+Daniel-Alcantara@users.noreply.github.com>
2022-12-13 14:05:57 -0300

    finalização Desktop

--- script.js
+++ script.js
@@ -43,6 +43,17 @@ $(document).ready(function () {
         $(".form_2 h3").css("color", "white")
         $(".form_3").css("background-color", "#bee2f8")
         $(".form_3 h3").css("color", "#002248")
+
+        if (plano_mes_ou_anual == "(Yearly)") {
+            $(".box_check_1 .value_checkbox p").html("+$10/yr")
+            $(".box_check_2 .value_checkbox p").html("+$20/yr")
+            $(".box_check_3 .value_checkbox p").html("+$20/yr")
+        }
+        if (plano_mes_ou_anual != "(Yearly)") {
+            $(".box_check_1 .value_checkbox p").html("+$1/mo")
+            $(".box_check_2 .value_checkbox p").html("+$2/mo")
+            $(".box_check_3 .value_checkbox p").html("+$2/mo")
+        }
     })
 
     $(".return_page2").click(function () {
@@ -111,6 +122,9 @@ $(document).ready(function () {
         valor_storage = 0
         valor_customizable = 0
         numeral_valor_selecionado = 0
+        online_service = "false"
+        large_storage = "false"
+        customizable_profile = "false"
     })
     // fim change.
 
@@ -119,7 +133,7 @@ $(document).ready(function () {
         plano_selecionado = $(this).find(".box_option_title h4").html()
         valor_plano_selecionado = $(this).find(".box_option_title p").html()
         $(this).css("border", "1px solid #726bc0");
-        
+
 
         $('.box_render').css("border", "1px solid #726bc0").not(this).css("border", "1px solid #eeee");
 
@@ -148,7 +162,6 @@ $(document).ready(function () {
     // check qual serviço adicional foi selecionado.
 
     $(".check_box_design").click(function () {
-        console.log(this)
         if ($(this).css("border-color") == ("rgb(114, 107, 192)")) {
             $(this).css("border", "1px solid #eeee")
             $(this).css("background-color", "unset")
@@ -170,17 +183,35 @@ $(document).ready(function () {
             $(this).css("border", "1px solid #726bc0")
             $(this).css("background-color", "#eeee")
             $(this).find(".input_check input").prop('checked', true)
-            if ($(this).find(".text_checkbox h4").html() == "Online service") {
+            if ($(this).find(".text_checkbox h4").html() == "Online service" && plano_mes_ou_anual == "(Monthly)") {
                 online_service = "true"
                 valor_online = 1
+                console.log(valor_online)
             }
-            if ($(this).find(".text_checkbox h4").html() == "Large storage") {
+            if ($(this).find(".text_checkbox h4").html() == "Online service" && plano_mes_ou_anual == "(Yearly)") {
+                online_service = "true"
+                valor_online = 10
+                console.log(valor_online)
+            }
+            if ($(this).find(".text_checkbox h4").html() == "Large storage" && plano_mes_ou_anual == "(Monthly)") {
                 large_storage = "true"
                 valor_storage = 2
+                console.log(valor_storage)
             }
-            if ($(this).find(".text_checkbox h4").html() == "Customizable profile") {
+            if ($(this).find(".text_checkbox h4").html() == "Large storage" && plano_mes_ou_anual == "(Yearly)") {
+                large_storage = "true"
+                valor_storage = 20
+                console.log(valor_storage)
+            }
+            if ($(this).find(".text_checkbox h4").html() == "Customizable profile" && plano_mes_ou_anual == "(Monthly)") {
                 customizable_profile = "true"
                 valor_customizable = 2
+                console.log(valor_customizable)
+            }
+            if ($(this).find(".text_checkbox h4").html() == "Customizable profile" && plano_mes_ou_anual == "(Yearly)") {
+                customizable_profile = "true"
+                valor_customizable = 20
+                console.log(valor_customizable)
             }
 
         }
@@ -218,15 +249,43 @@ $(document).ready(function () {
 
         if (online_service == "true") {
             $("#online").css("display", "block")
-            $("#value_online").css("display", "block")
+            if (plano_mes_ou_anual == "(Yearly)") {
+
+                $("#value_online").css("display", "block")
+                $("#value_online").html("+$10/yr")
+
+            }
+            else {
+                $("#value_online").css("display", "block")
+                $("#value_online").html("+$1/mo")
+
+
+            }
         }
         if (large_storage == "true") {
             $("#storage").css("display", "block")
-            $("#value_storage").css("display", "block")
+            if (plano_mes_ou_anual == "(Yearly)") {
+
+                $("#value_storage").css("display", "block")
+                $("#value_storage").html("+$20/yr")
+
+            }
+            else {
+                $("#value_storage").css("display", "block")
+                $("#value_storage").html("+$2/mo")
+
+            }
         }
         if (customizable_profile == "true") {
             $("#customizable").css("display", "block")
-            $("#value_customizable").css("display", "block")
+            if (plano_mes_ou_anual == "(Yearly)") {
+                $("#value_customizable").css("display", "block")
+                $("#value_customizable").html("+$20/yr")
+            }
+            else {
+                $("#value_customizable").css("display", "block")
+                $("#value_customizable").html("+$2/mo")
+            }
         }
         if (online_service == "false") {
             $("#online").css("display", "none")
